899287a95ef06b4fe8cd85ea83ce17c48d26e2ee
pass0 <pass0@DESKTOP-GKPDKEG>
2022-04-08 15:13:47 +0900

    연습

--- src/Cho1/C01HelloWorld.java
+++ src/Cho1/C01HelloWorld.java
@@ -1,10 +1,13 @@
-package Cho1;
+package Cho1; // 패키지명(폴더와 유사)
 
-public class C01HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+public class C01HelloWorld // 클래스 영역 - 객체 지향 구현단위
+{
 
+	public static void main(String[] args) // Main 매서드 영역 - 절차지향 구현 단위
+	{
+		System.out.println("Hello World!!"); //콘솔 화면 출력 메서드(기본제공 출력메서드)
+          // ; - 세미콜론 : 실행문의 마지막,마침표를 의미
 	}
 
 }
