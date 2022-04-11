02a1018ebee14cd65ad713d305488bf176932e53
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-04-11 10:13:53 +0900

    20220411 10:13

---
+++ src/Ch02/C03자료형.java
@@ -0,0 +1,16 @@
+package Ch02;
+
+public class C03자료형 {
+
+	public static void main(String[] args) {
+		
+		int num1;
+		num1=10;
+		int num2=4;
+		int num3 = num1 + num2;
+		System.out.println(num3);
+		
+
+	}
+
+}
