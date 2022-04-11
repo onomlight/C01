c404d88c5ae6addb42717cbdc890d4513d8d842e
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-04-11 11:14:37 +0900

    11:14

--- src/Ch02/C03자료형.java
+++ src/Ch02/C03자료형.java
@@ -3,12 +3,16 @@ package Ch02;
 public class C03자료형 {
 
 	public static void main(String[] args) {
+		//declaration : 선언  이름만 부여(오늘부터 다이어트할래~!/ 강제력이없음 / 안지킴)
+		//definition  : 정의  이름부여+공간형성( 지켜야함 )
+		//initialize  : 초기화 이름부여+공간형성+기본값저장
 		
-		int num1;
-		num1=10;
-		int num2=4;
-		int num3 = num1 + num2;
-		System.out.println(num3);
+		// 함수 선언부와 정의부가 나눠져있는걸 참고해야함 
+		int num1; // 변수를 정의 4byte 정수 저장 공간을 지정한 뒤 num1 라는 이름을 부여 (변수를 정의)
+		num1=10; // 10상수값을 상수풀에 저장한뒤 num1공간에 대입
+		int num2=4; // 4라는 값을 상수값을 상수풀에 저장한뒤 4byte 정수 num2공간에 초기화
+		int num3 = num1 + num2; // num1값과 num2값의 합을 num3 변수 공간에 초기화
+		System.out.println(num3); // num3안의 값을 출력
 		
 
 	}
---
+++ src/Ch02/C04자료형.java
@@ -0,0 +1,78 @@
+package Ch02;
+
+public class C04자료형 {
+
+	public static void main(String[] args) {
+//		 // int 자료형
+//		int var1 = 0b1111; // 2진수
+//		int var2 = 0206;  //8진수
+//		int var3 = 365;  //10진수
+//		int var4 = 0xb3;    //16진수    
+//		// 비트열로 저장됨 " "문자열 + 자료형
+//		System.out.println("var1 :" + var1);
+//		System.out.println("var2 :" + var2);
+//		System.out.println("var3 :" + var3);
+//		System.out.println("var4 :" + var4);
+//		
+
+		//----------------------------------------
+		//byte 자료형(1byte)
+//		byte var1 = -128;
+//		byte var2 = -30;
+//		byte var3 = 30;
+//		byte var4 = 127;
+//
+//		System.out.println("var1 :" + var1);
+//		System.out.println("var2 :" + var2);
+//		System.out.println("var3 :" + var3);
+//		System.out.println("var4 :" + var4);
+		
+		//----------------------
+		// char 자료형 (2byte - 음수값 x 16bit 0~65535 까지 넣을수있음)
+//		char var1 = 65535;
+//		char var2 = 5000;
+//		char var3 = 400;
+//		char var4 = 30;
+//	
+//		// 기계는 문자를 이해하지못해서 미리 약속된 언어가 표시됨
+//		 // (int)로 선언하면 문자로 표현됨 
+//		System.out.println("var1 :" + (int)var1);
+//		System.out.println("var2 :" + (int)var2);
+//		System.out.println("var3 :" + (int)var3);
+//		System.out.println("var4 :" + (int)var4);
+		
+		//--------------
+		// short 자료형(2byte - 음수값 o ) 약 -32768 ~ 32767
+		
+//		short var1 = 30001;
+//		short var2 = -30000;
+//		short var3 = -1234;
+//		short var4 = 4567;
+//		
+//		
+//	
+//		
+//		System.out.println("var1 :" + var1);
+//		System.out.println("var2 :" + var2);
+//		System.out.println("var3 :" + var3);
+//		System.out.println("var4 :" + var4);
+		
+		
+		// -------
+		
+		// long 자료형(8byte - 음수값 0) -21억~21억
+		//------------------------
+		// 리터널 상수 L 이라는 접미사를 붙여주면 int 기본 자료를 넣고, var1이라는 공간에다 복사에해서 사용
+//		long var1 = 2000000000L;
+//		long var2 = 100;
+//		long var3 = 100;
+//		long var4 = 100;
+//		
+//		
+//		System.out.println("var1 :" + var1);
+//		System.out.println("var2 :" + var2);
+//		System.out.println("var3 :" + var3);
+//		System.out.println("var4 :" + var4);
+	}
+
+}
