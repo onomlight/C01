5755c522363a628c634fce72b55c0ff0bd1b5c79
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-04-20 17:48:07 +0900

    ㅂㅈㄷㄱㅂㅈㄷㄱ

--- src/Ch19/C02StaticMain.java
+++ src/Ch19/C02StaticMain.java
@@ -15,13 +15,19 @@ class Rectangle
 	}
 	//끝점확인 기능
 	void ShowEndPoint() {
-		
+		System.out.printf("(%D,%d)\n",(x+width),(y+height));
+		// 기준점 x 에서 넓이만큼 더해주면 endpoint까지 가는 부분 
 	}
 	// 시작점,너비,높이,끝점확인
 	void ShowInfo() {
-		System.out.printf("X의 시작점 = %d Y의 시작점 = %d\n",x, y);
-		System.out.print("너비 :" + width);
-		System.out.print("높이 :" + height);
+				int endx=x+width;
+				int endy=y+height;
+		System.out.printf("시작-끝점 : (%d,%d) - (%d,%d)\n",x,y,endx,endy);
+		System.out.println("너비,높이 : " + width + " , " + height);
+		System.out.println("넓이 : " + (width + height));
+//		System.out.printf("X의 시작점 = %d Y의 시작점 = %d\n",x, y);
+//		System.out.print("너비 :" + width);
+//		System.out.print("높이 :" + height);
 	}
 }
 public class C02StaticMain {
@@ -36,9 +42,9 @@ public class C02StaticMain {
 		Rectangle rec2 = new Rectangle(5,7);
 		Rectangle rec3 = new Rectangle(6,5);
 		
-		rec1.ShowInfo();
-		rec2.ShowInfo();
-		rec3.ShowInfo();
+		rec1.ShowInfo();System.out.println();
+		rec2.ShowInfo();System.out.println();
+		rec3.ShowInfo();System.out.println();
 	}
 
 }
---
+++ src/Ch19/C03StaticMain.java
@@ -0,0 +1,14 @@
+package Ch19;
+
+public class C03StaticMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1 편의점 공통 할인율 (할인율 - static)
+		
+		// 2 레스토랑 예약하기 ( 예약가능 Room수 - static)
+		// 정우균이 00레스토랑에 예약하다
+	}
+
+}
